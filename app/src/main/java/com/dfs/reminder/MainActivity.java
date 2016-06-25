a8a5d8ad0afa39316d4eaa6c0d07bd7c319c4e11
breez.dp@gmail.com <y8z3qUr5k>
2016-06-25 19:20:44 +0300

    Added Toolbar & search button

--- app/src/main/java/com/dfs/reminder/MainActivity.java
+++ app/src/main/java/com/dfs/reminder/MainActivity.java
@@ -2,15 +2,30 @@ package com.dfs.reminder;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by Sam on 6/25/2016.
- */
+
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 }
