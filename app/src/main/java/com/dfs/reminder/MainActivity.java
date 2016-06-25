7504b848588f78308f5c772ec1c8ab14ae4dbd1c
breez.dp@gmail.com <y8z3qUr5k>
2016-06-25 20:57:26 +0300

    Add Navigation View & this.transparent color statusBar

--- app/src/main/java/com/dfs/reminder/MainActivity.java
+++ app/src/main/java/com/dfs/reminder/MainActivity.java
@@ -1,15 +1,16 @@
 package com.dfs.reminder;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
